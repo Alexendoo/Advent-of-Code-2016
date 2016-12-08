447a7083503fccf87c406387c26d057fae548922
Alex Macleod <alex@macleod.io>
2016-12-08 12:36:30 +0000

    Day 08 part 1

---
+++ Day 08/part-1.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+board = Array.new(50) { Array.new(8, 0) }
+
+ARGF.map(&:split).each do |args|
+  case args[0]
+  when 'rect'
+    x, y = args[1].split(?x).map(&:to_i)
+    for i in 0...x
+      for j in 0...y
+        board[i][j] = 1
+      end
+    end
+  when 'rotate'
+    board = board.transpose if args[1] == 'row'
+
+    rotate = args[2].split(?=).last.to_i
+    by = -args[4].to_i
+    board[rotate].rotate!(by)
+
+    board = board.transpose if args[1] == 'row'
+  end
+end
+
+p board.flatten.reduce(:+)
