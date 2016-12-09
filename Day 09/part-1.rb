20d67bc9796ec90350c735618caed031afaa2983
Alex Macleod <alex@macleod.io>
2016-12-09 23:17:45 +0000

    Day 09 part 1: Don't store decompressed output

--- Day 09/part-1.rb
+++ Day 09/part-1.rb
@@ -1,17 +1,17 @@
 #!/usr/bin/env ruby
 
 chars = ARGF.first.strip.chars
-out = []
+out = 0
 
 while chars.length > 0
   char = chars.shift
   if char == '('
     length, count = chars.shift(chars.index(')') + 1).join.scan(/\d+/).map(&:to_i)
-    repeat = chars.shift(length)
-    count.times { out.concat repeat }
+    length = chars.shift(length).length
+    out += length * count
   else
-    out.push char
+    out += 1
   end
 end
 
-p out.length
+p out
